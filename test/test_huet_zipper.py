e5c6aca05248cf76b173a1c5a3c8dacdc334fe99
BuildTools <unconfigured@null.spigotmc.org>
2021-07-15 22:03:38 -0400

    added testing of move up being permitted and not being permitted;

--- test/test_huet_zipper.py
+++ test/test_huet_zipper.py
@@ -45,33 +45,42 @@ _zipper = Zipper(
 
 _leaf_visits_per_leaf_value = {}
 _visits_total = 0
-for _index in range(1000000):
-    _choice = random.randrange(2) + 1
-    if _choice == 0:
-        _zipper = _zipper.move_up()
-    elif _choice == 1:
-        _zipper = _zipper.move_left()
-        if _zipper.is_focus_leaf():
-            _value = _zipper.get_leaf_value()
-            if _value not in _leaf_visits_per_leaf_value:
-                _leaf_visits_per_leaf_value[_value] = 0
-            _leaf_visits_per_leaf_value[_value] += 1
-            _visits_total += 1
-            _zipper = Zipper(
-                focus=_zipper.unfocus()
-            )
-    elif _choice == 2:
-        _zipper = _zipper.move_right()
-        if _zipper.is_focus_leaf():
-            _value = _zipper.get_leaf_value()
-            if _value not in _leaf_visits_per_leaf_value:
-                _leaf_visits_per_leaf_value[_value] = 0
-            _leaf_visits_per_leaf_value[_value] += 1
-            _visits_total += 1
-            _zipper = Zipper(
-                focus=_zipper.unfocus()
-            )
 
-for _value in _leaf_visits_per_leaf_value.keys():
-    _average_visits = _leaf_visits_per_leaf_value[_value]/_visits_total
-    print(f"Value average: {_value} was visited {_average_visits}")
+for _is_move_up_permitted in (True, False):
+    if _is_move_up_permitted:
+        print("Move up permitted")
+    else:
+        print("Move up not permitted")
+    for _index in range(100000):
+        if _is_move_up_permitted:
+            _choice = random.randrange(3)
+        else:
+            _choice = random.randrange(2) + 1
+        if _choice == 0:
+            _zipper = _zipper.move_up()
+        elif _choice == 1:
+            _zipper = _zipper.move_left()
+            if _zipper.is_focus_leaf():
+                _value = _zipper.get_leaf_value()
+                if _value not in _leaf_visits_per_leaf_value:
+                    _leaf_visits_per_leaf_value[_value] = 0
+                _leaf_visits_per_leaf_value[_value] += 1
+                _visits_total += 1
+                _zipper = Zipper(
+                    focus=_zipper.unfocus()
+                )
+        elif _choice == 2:
+            _zipper = _zipper.move_right()
+            if _zipper.is_focus_leaf():
+                _value = _zipper.get_leaf_value()
+                if _value not in _leaf_visits_per_leaf_value:
+                    _leaf_visits_per_leaf_value[_value] = 0
+                _leaf_visits_per_leaf_value[_value] += 1
+                _visits_total += 1
+                _zipper = Zipper(
+                    focus=_zipper.unfocus()
+                )
+
+    for _value in _leaf_visits_per_leaf_value.keys():
+        _average_visits = _leaf_visits_per_leaf_value[_value]/_visits_total
+        print(f"Value average: {_value} was visited {_average_visits}")
